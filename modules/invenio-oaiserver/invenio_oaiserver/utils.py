954d47128f4729995e5e48e4359d3fab045305d7
ivis-enami <shoya.enami@ivis.co.jp>
2024-02-08 16:46:54 +0900

    コミットテスト２

--- modules/invenio-oaiserver/invenio_oaiserver/utils.py
+++ modules/invenio-oaiserver/invenio_oaiserver/utils.py
@@ -239,6 +239,7 @@ def get_index_state():
 def is_output_harvest(path_list, index_state):
     def _check(index_id):
         print('test')
+        print('test')
         if index_id in index_state:
             if not index_state[index_id]['parent'] \
                     or index_state[index_id]['parent'] == '0':
